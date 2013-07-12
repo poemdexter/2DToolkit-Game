3f47b1d7ef2e8d7e106ae26e9f8afd485bd0686b
poemdexter <poemdexter@gmail.com>
2013-07-11 19:33:01 -0500

    whoa buddy RPC calls and smooooth mooooves

--- Assets/Assets/Scripts/PlayerMovement.cs
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -7,32 +7,51 @@ public class PlayerMovement : MonoBehaviour
 	public float jumpPower = 100.0f;
 	private bool canJump = false;
 	private bool canMove = true;
+	private Vector3 movement;
+	private Vector3 force;
 	
 	// Use this for initialization
 	void Start () {}
 	
 	// Update is called once per frame
 	void Update () 
-	{
+	{	
 		if(networkView.isMine)
 		{
+			movement = Vector3.zero;
 			if (canMove)
 			{
 				if (Input.GetKeyDown(KeyCode.Space) && canJump)
 				{
-					rigidbody.AddForce(Vector3.up * jumpPower);
+					force = Vector3.up * jumpPower;
 					canJump = false;
 				}
 				if (Input.GetKey(KeyCode.A))
 				{
-					transform.position += Vector3.left * speed * Time.deltaTime;
+					movement += Vector3.left * speed * Time.deltaTime;
 				}
 				if (Input.GetKey(KeyCode.D))
 				{
-					transform.position += Vector3.right * speed * Time.deltaTime;
+					movement += Vector3.right * speed * Time.deltaTime;
 				}
+				
+				networkView.RPC("TellTheWorld", RPCMode.OthersBuffered, movement, force);
 			}
 		}
+		
+		if (movement != Vector3.zero || force != Vector3.zero)
+		{
+			transform.position += movement;
+			rigidbody.AddForce(force);
+			force = Vector3.zero;
+		}
+	}
+	
+	[RPC]
+	void TellTheWorld(Vector3 myMovement, Vector3 myForce)
+	{
+		movement = myMovement;
+		force = myForce;
 	}
 	
 	void OnCollisionEnter(Collision collision)
