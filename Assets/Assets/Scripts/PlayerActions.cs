66c6056e4e42b8abf6c359270d111800cf63878c
poemdexter <poemdexter@gmail.com>
2013-07-21 22:52:58 -0500

    random chests spawning now.

--- Assets/Assets/Scripts/PlayerActions.cs
+++ Assets/Assets/Scripts/PlayerActions.cs
@@ -43,7 +43,7 @@ public class PlayerActions : MonoBehaviour
 		if(collider.tag == "Treasure")
 		{
 			attemptedChest = collider.GetComponent<ChestController>();
-			canOpenChest = (attemptedChest.GetChestState == ChestState.closed) ? true : false;
+			canOpenChest = (attemptedChest.GetChestState() == ChestState.closed) ? true : false;
 		}
 	}
 	
