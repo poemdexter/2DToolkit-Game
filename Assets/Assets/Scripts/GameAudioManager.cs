b4f87a972a4532246bef7529783896d80cefd71f
poemdexter <poemdexter@gmail.com>
2013-07-31 00:50:36 -0500

    bilinear filtering and waiting for server music

--- Assets/Assets/Scripts/GameAudioManager.cs
+++ Assets/Assets/Scripts/GameAudioManager.cs
@@ -5,16 +5,35 @@ public class GameAudioManager : MonoBehaviour {
 	
 	bool startMusic = false;
 	bool startedMusic = false;
+	bool startMusic2 = false;
+	bool startedMusic2 = false;
 	
 	public float musicVolume = 1;
 	
+	public AudioSource playingMusic;
+	public AudioSource waitingMusic;
+	
 	// Update is called once per frame
 	void Update () {
-		if (PlayerInfo.gameStarted) startMusic = true;
+		
+		if (!PlayerInfo.gameStarted) 
+			startMusic2 = true;
+		if (startMusic2 && !startedMusic2)
+		{
+			waitingMusic.volume = musicVolume;
+			waitingMusic.Play();
+			startedMusic2 = true;
+		}
+		
+		if (PlayerInfo.gameStarted)
+		{
+			startMusic = true;
+			waitingMusic.Stop();
+		}
 		if (startMusic && !startedMusic)
 		{
-			audio.volume = musicVolume;
-			audio.Play();
+			playingMusic.volume = musicVolume;
+			playingMusic.Play();
 			startedMusic = true;
 		}
 	}
